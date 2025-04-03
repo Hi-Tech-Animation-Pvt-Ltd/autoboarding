5ddcaf7105e1cce25224869d2f4fa95fc13a8b74
Tumin <29175230+TumAro@users.noreply.github.com>
2025-04-03 15:34:51 +0530

    restarting

--- __init__.py
+++
@@ -1,7 +0,0 @@
-from krita import *
-from . import main
-
-# This line tells Krita about our plugin
-Krita.instance().addExtension(main.AutoBoardingExtension(Krita.instance()))
-def createPlugin(parent):
-    return main.AutoBoardingExtension(parent)
